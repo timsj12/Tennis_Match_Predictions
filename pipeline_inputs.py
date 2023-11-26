0c4e27b216df0cc92a2f50888eaa9ef07353b602
timj90 <timj90@vt.edu>
2023-11-25 21:06:32 -0500

    Setting DataSet Inputs to create multiple pipelines

---
+++ pipeline_inputs.py
@@ -0,0 +1,32 @@
+
+"""
+Comment out data sets that are not desired to be examined below.
+Maximum of four pipelilines to be created
+
+Men's pre match data
+Men's post match data
+
+Women's pre match data
+Women's post match data
+"""
+
+
+def set_dataset_inputs(**kwargs):
+
+    predictions = []
+
+    # Look at Men's Tennis Data and make predictions on post match data
+    predictions.append('atp_post')
+
+    # Look at Men's Tennis Data and make predictions on matchup only and no match metrics
+    predictions.append('atp_pre')
+
+    # Look at Men's Tennis Data and make predictions on post match data
+    predictions.append('wta_post')
+
+    # Look at Men's Tennis Data and make predictions on matchup only and no match metrics
+    predictions.append('wta_pre')
+
+    ti = kwargs['ti']
+    ti.xcom_push(key='datasets', value=predictions)
+
